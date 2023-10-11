0aacb3b1f1c31b25c9c8c3e7dbd1dcb7cfc5bbb4
IvanTsybulko <107126601+IvanTsybulko@users.noreply.github.com>
2023-10-11 21:43:07 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> problem = Console.ReadLine().Split(' ').ToList();
+
+            //11 * 22 + 33 * 11 / 22 + 11                   = 269.5
+            //24 + 15 - 21 * 34 / 24 + 12 * 12 + 14 / 14    = 154.25
+            //15 + ( 18 + 13 * 8 ) + 2                      = 139
+            //15 + ( 18 + 13 * 8 ) + ( 8 + 9 ) + 2          = 156
+            //15 + ( 8 * ( 18 + 13 * 8 ) - 9 ) + 2          = 984
+
+
+            Console.WriteLine(calculate(problem));
+        }
+
+        private static double calculate(List<string> problem)
+        {
+            problem = solveBrackets(problem); //solves everithing in brackets
+                                              //and replaces it with theresult in the original problem
+
+            problem = solveMultAndDiv(problem); //solves every operation with * or /
+                                                //and replaces it with the result in the original problem
+
+            problem = solveAddAndMin(problem); //solves every + or - operation and replaces it with the
+                                               //result in the original problem
+
+            return double.Parse(problem[0]);
+        }
+
+        private static List<string> solveBrackets(List<string> problem)
+        {
+            while(problem.Contains("("))
+            {
+                int openbracket = -1;
+                int closeBracket = -1;
+
+                for (int i = 0; i < problem.Count; i++)
+                {
+                    if (problem[i] == "(")
+                    {
+                        openbracket = i;
+                    }
+
+                    if (problem[i] == ")")
+                    {
+                        closeBracket = i;
+                        break;
+                    }
+                }
+
+                List<string> inBracketsProblem = problem.Skip(openbracket + 1).Take(closeBracket - openbracket - 1).ToList();
+                inBracketsProblem = solveMultAndDiv(inBracketsProblem);
+                inBracketsProblem = solveAddAndMin(inBracketsProblem);
+
+                problem[openbracket] = inBracketsProblem[0];
+
+                problem.RemoveRange(openbracket + 1, closeBracket - openbracket);
+            }
+
+            return problem;
+        }
+
+        private static List<string> solveMultAndDiv(List<string> problem)
+        {
+            for (int i = 0; i < problem.Count - 1; i++)
+            {
+                if (problem[i + 1] == "*" || problem[i + 1] == "/")
+                {
+                    if (problem[i + 1] == "*")
+                    {
+                        double result = double.Parse(problem[i]) * double.Parse(problem[i + 2]);
+
+                        problem[i] = result.ToString();
+                        problem.RemoveAt(i + 1);
+                        problem.RemoveAt(i + 1);
+                        i--;
+                    }
+                    else
+                    {
+                        double result = double.Parse(problem[i]) / double.Parse(problem[i + 2]);
+
+                        problem[i] = result.ToString();
+                        problem.RemoveAt(i + 1);
+                        problem.RemoveAt(i + 1);
+                        i--;
+                    }
+                }
+            }
+
+            return problem;
+        }
+
+        private static List<string> solveAddAndMin(List<string> problem)
+        {
+            for (int i = 0; i < problem.Count - 1; i++)
+            {
+                if (problem[i + 1] == "+" || problem[i + 1] == "-")
+                {
+                    if (problem[i + 1] == "+")
+                    {
+                        double result = double.Parse(problem[i]) + double.Parse(problem[i + 2]);
+
+                        problem[i] = result.ToString();
+                        problem.RemoveAt(i + 1);
+                        problem.RemoveAt(i + 1);
+                        i--;
+                    }
+                    else
+                    {
+                        double result = double.Parse(problem[i]) - double.Parse(problem[i + 2]);
+
+                        problem[i] = result.ToString();
+                        problem.RemoveAt(i + 1);
+                        problem.RemoveAt(i + 1);
+                        i--;
+                    }
+                }
+            }
+
+            return problem;
+        }
+
+        //double identifyBrackets(List<string> problem)
+        //{
+        //    List<string> problemBeweenBrackets = new List<string>();
+
+        //    bool betweenBrackets = false;
+
+        //    foreach (var item in problem)
+        //    {
+        //        if (item == ")")
+        //        {
+        //            betweenBrackets = false;
+        //        }
+
+        //        if (betweenBrackets)
+        //        {
+        //            problemBeweenBrackets.Add(item);
+        //        }
+
+        //        if (item == "(")
+        //        {
+        //            betweenBrackets = true;
+        //        }
+        //    }
+
+
+        //}
+    }
+}
